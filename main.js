a81047dce1377bf8478418401c5b0f0a04fc6c5f
Matheus Mendes <matheus_mendess@live.com>
2022-02-11 02:09:46 -0300

    parcialmente finalizado

--- main.js
+++ main.js
@@ -48,28 +48,25 @@ function criarPartida(parida){
 
     return div;
 }
-
 fetchPartida('https://api.opendota.com/api/players/159992731/recentMatches');
 
+async function fetchPerfil(url){
+    const perfilResponse = await fetch(url);
+    const perfilJSON = perfilResponse.json();
+    const perfilGrid = document.querySelector('.meu-perfil');
 
-
-function myGet (url){
-    let resquest = new XMLHttpRequest();
-    resquest.open("GET", url, false);
-    resquest.send();
-    return resquest.responseText;
-}
-
-function main(){
-    let dadosPerfil = (myGet("https://api.opendota.com/api/players/159992731"));
-
-    let perfil = JSON.parse(dadosPerfil);
-
-    document.getElementById("demo").innerHTML = "Nick na steam: " + perfil.profile.personaname + "<br> ultima partida: " + perfil.profile.last_login + "<br> Id Dota2: " + 
-    perfil.profile.account_id + "<br>partidas ganhas: "+ perfil.profile.win;
+    perfilJSON.forEach(perfil => {
+        const divPerfil = criarPerfil(perfil);
+        perfilGrid = appendChild(divPerfil);
+    });
 
 }
 
+function criarPerfil(perfil){
+    const div = document.createElement('div');
+    div.classList.add('meu-perfil');
 
+    console.log(perfil);
+}
 
-main()
+fetchPerfil('https://api.opendota.com/api/players/159992731');
