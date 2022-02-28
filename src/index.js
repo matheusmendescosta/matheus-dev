25af6eb8f50b4f6262d5a18e34fefb34b21f8900
Matheus Mendes <matheus_mendess@live.com>
2022-02-28 16:28:40 -0300

    alterações nos index html e js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Navbar from './components/Navbar/Navbar';
+import Header from './components/Header/Header'
+import AlertContent from './components/AlertContent/AlertContent';
+import ContentCards from './components/ContentCards/ContentCards';
+import Footer from './components/Footer/Footer';
 
 ReactDOM.render(
   <React.StrictMode>
     <Navbar />
+    <Header />
+    <AlertContent />
+    <ContentCards />
+    <Footer />
   </React.StrictMode>,
   document.getElementById('root')
 );
